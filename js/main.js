a60787de18fcfa45d3f90defafb18df37e74f16d
dynamiczny <marcinpawlicki0@gmail.com>
2019-05-27 21:36:51 +0200

    added changable bg dependent on the weather

--- js/main.js
+++ js/main.js
@@ -62,6 +62,7 @@ window.onload = function(){
                 document.querySelector('.date').innerHTML = `${json.location.localtime}`;
                 document.querySelector('.weather-visuality>p').innerHTML = json.current.condition.text;
                 document.querySelector('.weather-visuality>img').src = json.current.condition.icon;
+                changeBg(document.querySelector('.weather-visuality>img').src);
                 document.querySelector('h1').innerHTML = `${Math.round(json.current.temp_c)}°`;
                 document.querySelector('.highest-temp').innerHTML = `${Math.round(json.forecast.forecastday[0].day.maxtemp_c)}° C`;            
                 document.querySelector('.lowest-temp').innerHTML = `${Math.round(json.forecast.forecastday[0].day.mintemp_c)}° C`;
@@ -171,5 +172,103 @@ window.onload = function(){
         return replaced;
     }
 
+    function changeFontColor(color){
+        document.querySelector('body').style.color = `${color}`;
+        document.querySelector('.city-list').style.color = `white`;
+        if(color == 'black'){
+            document.querySelector('.hamburger').classList.remove('whiteburger')
+            document.querySelector('.add-to-fav').classList.remove('whiteplus')
+            document.querySelector('.hamburger').classList.add('blackburger')
+            document.querySelector('.add-to-fav').classList.add('blackplus')
+            document.querySelector('.line').classList.add('blackline')
 
+        }
+        if(color == 'white'){
+            document.querySelector('.hamburger').classList.remove('blackburger')
+            document.querySelector('.add-to-fav').classList.remove('blackplus')
+            document.querySelector('.line').classList.remove('blackline')
+            document.querySelector('.hamburger').classList.add('whiteburger')
+            document.querySelector('.add-to-fav').classList.add('whiteplus')
+
+        }
+    }
+
+    function changeBg(src){
+        //weather at night
+        let regex = /night/gi;
+        if(regex.test(src)){
+            //thunders at night
+            console.log('o')
+            regex = /(200)|(386)|(389)|(392)|(395)/gi;
+            if(regex.test(src)){
+                document.querySelector('body').style.background = 'linear-gradient(-0deg, #fbff31b3 -200%, #1a2530 50%,#11171d 100%) fixed'
+                changeFontColor('white')
+                return;
+            }
+
+            document.querySelector('body').style.background = 'linear-gradient(-20deg, #2b5876 0%, #4e4376 100%) fixed'
+            changeFontColor('white')
+            return;
+        } else{
+            //sunny
+            regex = /(113)/gi;
+            if(regex.test(src)){
+                document.querySelector('body').style.background = 'linear-gradient(to top, #fddb92 0%, #d1fdff 100%) fixed'
+                changeFontColor('black')
+                return;
+            }
+            //sun with clouds
+            regex = /(116)/gi;
+            if(regex.test(src)){
+                document.querySelector('body').style.background = 'linear-gradient(-240deg, #5f9cff 0%, #c2e9fb 100%) fixed'
+                changeFontColor('black')
+                return;
+            }
+            //cloudy
+            regex = /(119)|(122)|(143)|(248)|(260)/gi;
+            if(regex.test(src)){
+                document.querySelector('body').style.background = 'linear-gradient(-225deg, #f5f7fa 0%, #c3cfe2 100%) fixed'
+                changeFontColor('black')
+                return;
+            }
+            //sun with rain
+            regex = /(176)|(182)|(293)|(299)|(305)|(353)|(356)|(359)|(362)|(365)|(374)|(377)/gi;
+            if(regex.test(src)){
+                document.querySelector('body').style.background = 'linear-gradient(to top, #0c3483 0%, #dfdaa2 130%) fixed'
+                changeFontColor('white')
+                return;
+            }
+            ///thunders
+            regex = /(200)|(386)|(389)|(392)|(395)/gi;
+            if(regex.test(src)){
+                document.querySelector('body').style.background = 'linear-gradient(-0deg, #fbff31b3 -200%, #303f4e 100%) fixed'
+                changeFontColor('white')
+                return;
+            }
+            //sun with snow
+            regex = /(179)|(323)|(329)|(335)|(368)|(371)/gi;
+            if(regex.test(src)){
+                document.querySelector('body').style.background = 'linear-gradient(to top, #6e6b6b 0%,#d8d8d8 70%, #f9f586 100%) fixed'
+                changeFontColor('black')
+                return;
+            }
+            //rain
+            regex = /(263)|(266)|(296)|(302)|(208)|(317)|(320)/gi;
+            if(regex.test(src)){
+                document.querySelector('body').style.background = 'linear-gradient(to top, #2bb5b6 0%, #424057 100%) fixed'
+                changeFontColor('white')
+                return;
+            }
+            //snow
+            regex = /(185)|(281)|(284)|(311)|(314)|(227)|(230)|(326)|(332)|(338)|(350)/gi;
+            if(regex.test(src)){
+                document.querySelector('body').style.background = 'linear-gradient(to top, #9dabcb 0%, #eef1f5 100%) fixed;'
+                changeFontColor('black')
+                return;
+            }
+            document.querySelector('body').style.background = 'linear-gradient(#1A2760, #9561A1) fixed'
+            changeFontColor('white')
+            return;
+        }
+    }
 }
